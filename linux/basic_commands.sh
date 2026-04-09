6b1295ac9365afe305186a153986b8c25a118ce2
Aiman-khan-tech <aiman.khan.tech@gmail.com>
2026-04-10 01:28:22 +0530

    Add basic Linux commands script (bash script)

---
+++ linux/basic_commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "===== DevOps Linux Basic Commands ====="
+
+echo "Current Directory:"
+pwd
+
+echo "Logged in User:"
+whoami
+
+echo "System Date:"
+date
+
+echo "System Uptime:"
+uptime
+
+echo "Memory Usage:"
+free -h
+
+echo "Disk Usage:"
+df -h
+
+echo "CPU Info:"
+lscpu
+
+echo "List Files:"
+ls -la
